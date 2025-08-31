6ebcac565557695bc2e170e2e6abdb556981000b
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-08-31 17:32:10 +0900

    fix: JWT KEY .env 분리, TTL 변수 정의

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -10,6 +10,10 @@ public class UserService {
     private final UserRepository userRepository = new UserRepository();
     private final JwtBuilder jwtBuilder = new JwtBuilder();
 
+    private final int TTL = 3600;
+
+    String jwtSecret = System.getenv("JWT_KEY");
+
     public boolean registerUser(User user) {
         String userId = user.userId;
         if (!userRepository.exists(userId) && userRepository.save(userId, user)) {
@@ -20,13 +24,13 @@ public class UserService {
     }
 
     public Optional<String> createAccessToken(String id, String pw) {
-        if(!userRepository.checkUser(id,pw)){
+        if (!userRepository.checkUser(id, pw)) {
             return null;
         }
         String accessToken = jwtBuilder
                 .userId(id)
-                .ttl(3600)
-                .secret("dwdd")
+                .ttl(TTL)
+                .secret(jwtSecret)
                 .build();
 
         return accessToken.describeConstable();
